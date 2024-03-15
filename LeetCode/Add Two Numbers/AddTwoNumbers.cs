e55dbabf076796b3a13677e1a60bd98b855a4879
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-03-15 22:36:15 +0300

    LeetCode - Add Two Numbers - beats 98% on memory usage - might be optimized for performance

---
+++ LeetCode/Add Two Numbers/AddTwoNumbers.cs
@@ -0,0 +1,33 @@
+﻿namespace Add_Two_Numbers
+{
+    public static class AddTwoNumbers
+    {
+        public static ListNode Solve(ListNode l1, ListNode l2)
+        {
+            var resultList = new ListNode();
+            var hasOverFlow = false;
+            var currentL1Node = l1;
+            var currentL2Node = l2;
+            var currentResultNode = resultList;
+
+            while (currentL1Node != null || currentL2Node != null || hasOverFlow)
+            {
+                var localSum = (currentL1Node?.val ?? 0) + (currentL2Node?.val ?? 0) + (hasOverFlow ? 1 : 0);
+
+                hasOverFlow = localSum > 9;
+                currentResultNode.val = hasOverFlow ? localSum % 10 : localSum;
+
+                if (currentL1Node?.next != null || currentL2Node?.next != null || hasOverFlow)
+                {
+                    currentResultNode.next = new ListNode();
+                    currentResultNode = currentResultNode?.next;
+                }
+
+                currentL1Node = currentL1Node?.next;
+                currentL2Node = currentL2Node?.next;
+            }
+
+            return resultList;
+        }
+    }
+}
---
+++ LeetCode/Add Two Numbers/ListNode.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Add_Two_Numbers
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder(val.ToString());
+            var nextNode = next;
+
+            while (nextNode is not null)
+            {
+                result.Append(nextNode.val.ToString());
+                nextNode = nextNode.next;
+            }
+
+            return result.ToString();
+        }
+    }
+
+}

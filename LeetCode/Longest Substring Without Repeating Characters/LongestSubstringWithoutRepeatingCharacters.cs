a598297f7a47d94df5020d1026208819f814bde3
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-03-16 23:34:52 +0300

    LeetCode - Longest Substring Without Repeating Characters - beats 99% on memory usage - 35% on runtime

---
+++ LeetCode/Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,47 @@
+﻿namespace Longest_Substring_Without_Repeating_Characters
+{
+    public static class LongestSubstringWithoutRepeatingCharacters
+    {
+        public static int Solve(string s)
+        {
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+
+            if (s.Length == 1)
+            {
+                return 1;
+            }
+
+            var maxLength = 0;
+            var startingIndex = 0;
+            var currentCharSet = new HashSet<char>();
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var hasCharacter = currentCharSet.Contains(s[i]);
+
+                if (hasCharacter)
+                {
+                    if (currentCharSet.Count > maxLength)
+                    {
+                        maxLength = currentCharSet.Count;
+                    }
+
+                    var prevStartingIndex = startingIndex;
+                    startingIndex = s.IndexOf(s[i], prevStartingIndex) + 1;
+
+                    for (var j = prevStartingIndex; j < startingIndex; j++)
+                    {
+                        currentCharSet.Remove(s[j]);
+                    }
+                }
+
+                currentCharSet.Add(s[i]);
+            }
+
+            return currentCharSet.Count > maxLength ? currentCharSet.Count : maxLength;
+        }
+    }
+}
---
+++ LeetCode/Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using Longest_Substring_Without_Repeating_Characters;
+
+Console.WriteLine(LongestSubstringWithoutRepeatingCharacters.Solve("abcabcbb"));
+Console.ReadLine();

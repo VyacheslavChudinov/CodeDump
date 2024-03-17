001e1fc043af28742fa70e2e4cbcbe29c5551204
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-03-17 21:48:54 +0300

    LeetCode -Two Sum - beats 96% on memory usage - 46% on runtime

---
+++ LeetCode/Two Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿using Two_Sum;
+
+var result = TwoSum.Solve([2, 7, 11, 15], 9);
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+Console.ReadLine();
---
+++ LeetCode/Two Sum/TwoSum.cs
@@ -0,0 +1,21 @@
+﻿namespace Two_Sum
+{
+    internal class TwoSum
+    {
+        public static int[] Solve(int[] nums, int target)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return [i, j];
+                    }
+                }
+            }
+
+            return [];
+        }
+    }
+}

d5a01860d49c315bf903c28a15fee79d9a047ee7
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-03-19 18:47:21 +0300

    LeetCode - Reverse Integer - Medium - initial

---
+++ LeetCode/Reverse Integer/Program.cs
@@ -0,0 +1,7 @@
+﻿using Reverse_Integer;
+
+Console.WriteLine(ReverseInteger.Solve(123));
+Console.WriteLine(ReverseInteger.Solve(2113847412));
+Console.WriteLine(ReverseInteger.Solve(2147483112));
+
+
---
+++ LeetCode/Reverse Integer/ReverseInteger.cs
@@ -0,0 +1,37 @@
+﻿namespace Reverse_Integer
+{
+    public static class ReverseInteger
+    {
+        public static string Limit = "2147483647"; // 7463847412
+        public static bool Solve(int value)
+        {
+
+            return CheckIfExceedsLimit(value);
+
+        }
+
+        public static bool CheckIfExceedsLimit(int value)
+        {
+            var valueAsString = value.ToString();
+
+            if (valueAsString.Length > Limit.Length)
+            {
+                return true;
+            }
+
+            if (valueAsString.Length == Limit.Length)
+            {
+                for (int i = 0; i < valueAsString.Length; i++)
+                {
+                    if (Limit[i] < valueAsString[valueAsString.Length - i - 1])
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            return false;
+        }
+    }
+}

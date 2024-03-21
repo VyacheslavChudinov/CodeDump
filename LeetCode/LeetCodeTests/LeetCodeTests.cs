3c152896536d2f518e4209f7ea49b3271a442aed
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-03-21 22:51:13 +0300

    feat: add tests for reverse integer library

--- LeetCode/LeetCodeTests/LeetCodeTests.cs
+++ LeetCode/LeetCodeTests/LeetCodeTests.cs
@@ -1,6 +1,7 @@
 
 
 using Longest_Palindromic_Substring;
+using Reverse_Integer;
 
 namespace LeetCodeTests
 {
@@ -8,13 +9,21 @@ namespace LeetCodeTests
     public class LeetCodeTests
     {
         [TestMethod]
-
         public void TestLongestPalindromicSubstring()
         {
             Assert.AreEqual(LongestPalindromicSubstring.Solve("a"), "a");
             Assert.IsTrue(LongestPalindromicSubstring.Solve("babad") == "bab" || LongestPalindromicSubstring.Solve("babad") == "aba");
             Assert.AreEqual(LongestPalindromicSubstring.Solve("cbbd"), "bb");
             Assert.AreEqual(LongestPalindromicSubstring.Solve("bb"), "bb");
+        }
+
+        [TestMethod]
+        public void TestReverseInteger()
+        {
+            Assert.AreEqual(ReverseInteger.Solve(-2147483412), -2143847412);
+            Assert.AreEqual(ReverseInteger.Solve(123), 321);
+            Assert.AreEqual(ReverseInteger.Solve(2000000000), 2);
+            Assert.AreEqual(ReverseInteger.Solve(2099999999), 0);
         }
     }
 }

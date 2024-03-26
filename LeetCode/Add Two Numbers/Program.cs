8ae72641009cd78fc2311f7c146366230be1dec0
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-03-26 17:31:34 +0300

    Change add two numbers project to class lib

--- LeetCode/Add Two Numbers/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Add_Two_Numbers;
-
-// 2 -> 4 -> 3
-var list1 = new ListNode(2, new ListNode(4, new ListNode(3, null)));
-var list2 = new ListNode(5, new ListNode(6, new ListNode(4, null)));
-Console.WriteLine(AddTwoNumbers.Solve(list1, list2).ToString());

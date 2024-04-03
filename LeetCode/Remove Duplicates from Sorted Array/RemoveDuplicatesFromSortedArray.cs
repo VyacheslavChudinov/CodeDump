985388c6933c60f57f487e8530e0898ac450757e
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-04-03 15:11:41 +0300

    LeetCode - Remove Duplicates from Sorted Array - 40% on runtime - 94% on memory

---
+++ LeetCode/Remove Duplicates from Sorted Array/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,29 @@
+﻿namespace Remove_Duplicates_from_Sorted_Array
+{
+    public static class RemoveDuplicatesFromSortedArray
+    {
+        public static int Solve(int[] nums)
+        {
+
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return 1;
+
+            var insertPosition = 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[insertPosition - 1])
+                {
+                    continue;
+                }
+                else
+                {
+                    nums[insertPosition] = nums[i];
+                    insertPosition++;
+                }
+            }
+
+            return insertPosition;
+        }
+    }
+}
